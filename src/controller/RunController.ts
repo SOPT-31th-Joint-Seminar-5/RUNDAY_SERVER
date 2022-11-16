5e0ddacb340fc9502101b157f15d42f5397311db
yujindonut <hyujin0825@gmail.com>
2022-11-16 23:42:17 +0900

    [FIXING] 좋아요 삭제 수정 사항
    좋아요 삭제 수정 사항

--- src/controller/RunController.ts
+++ src/controller/RunController.ts
@@ -14,6 +14,28 @@ const getRunInfo = async (req: Request, res: Response) => {
   }
 };
 
+const deleteLike = async (req: Request, res: Response) => {
+  const { runId } = req.params;
+  const { userId } = req.body;
+  
+  console.log(userId);
+
+  if(!runId || !userId) {
+    return res.status(404).json({ status: 404, message: "NOT_FOUND" });
+  }
+  try {
+    const data = await runService.deleteLike(+runId, +userId);
+    res.status(200).json({ status: 200, message: "좋아요 취소", data });
+  } catch (error) {
+    console.log("ERORROR", error)
+    if (error == 404) {
+      return res.status(404).json({ status: 404, message: "NOT_FOUND" });
+    }
+    res.status(500).json({ status: 500, message: "INTERNAL_SERVER_ERROR" });
+  }
+};
+
 export default {
   getRunInfo,
+  deleteLike,
 };
